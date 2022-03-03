f8ca851ac21d6a06a7e021738f311a07d43afa40
Malcolm Sparks <mal@juxt.pro>
2022-03-03 15:52:30 +0000

    checkpoint

--- test/juxt/pass/authorization_explainer_test.clj
+++ test/juxt/pass/authorization_explainer_test.clj
@@ -14,36 +14,61 @@
 
 ;; As a warmup, let's start with some contrived data and demonstrate how
 ;; authorization works in Site.
+
+;; In Site's authorization system, a subject might cause an effect on a resource
+;; (which would change it in some way). The authorization system must first
+;; check that the subject (user) is allowed to cause a given effect on the
+;; resource.
+
+;; A long time ago, web servers supported 'user directories'. If you had an
+;; account on a host and your username was 'alice', you could put files into a
+;; public_html directory in your home directory and this would be published over
+;; the WWW under http://host/~alice/. The tilde (~) indicates that the files
+;; belong to the account owner. See
+;; https://httpd.apache.org/docs/2.4/howto/public_html.html for further details.
+
+;; We'll create a similar system here, using subjects/effects/resources and
+;; scopes.
+
 (deftest warmup-test
   (submit-and-await!
    [
+    [:xtdb.api/put
+     {:xt/id "https://example.org/people/alice",
+      ::site/type "User"
+      ::username "alice"}]
+
+    [:xtdb.api/put
+     {:xt/id "https://example.org/people/bob",
+      ::site/type "User"
+      ::username "bob"}]
+
+    [:xtdb.api/put
+     {:xt/id "https://example.org/people/carl",
+      ::site/type "User"
+      ::username "carl"}]
+
     [::xt/put
-     {:xt/id "https://example.org/effects/put-user-dir-resource"
+     {:xt/id "https://example.org/effects/put-user-dir"
       ::site/type "Effect"
       ::pass/scope "userdir:write"
       ::pass/resource-matches "https://example.org/~([a-z]+)/.+"
       ::pass/effect-args [{}]}]
 
-    [:xtdb.api/put
-     {:xt/id "https://example.org/people/alice",
-      ::site/type "User"
-      ::username "alice"
-      :juxt.pass.alpha/ruleset "https://example.org/ruleset"}]
-
-    [:xtdb.api/put
-     {:xt/id "https://example.org/people/bob",
-      ::site/type "User"
-      ::username "bob"
-      :juxt.pass.alpha/ruleset "https://example.org/ruleset"}]
-
     [::xt/put
      {:xt/id "https://example.org/acls/alice-can-create-user-dir-content"
       ::site/type "ACL"
       ::pass/subject "https://example.org/people/alice"
-      ::pass/effect #{"https://example.org/effects/put-user-dir-resource"}
+      ::pass/effect #{"https://example.org/effects/put-user-dir"}
       ;; Is not constrained to a resource
-      ::pass/resource nil #_"https://example.org/people/"
-      }]])
+      ::pass/resource nil}]
+
+    [::xt/put
+     {:xt/id "https://example.org/acls/bob-can-create-user-dir-content"
+      ::site/type "ACL"
+      ::pass/subject "https://example.org/people/bob"
+      ::pass/effect #{"https://example.org/effects/put-user-dir"}
+      ::pass/resource nil}]])
 
   (let [check-acls
         (fn [db subject effect resource access-token-effective-scope]
@@ -77,16 +102,51 @@
 
         db (xt/db *xt-node*)]
 
-
     (are [subject effect resource access-token-effective-scope ok?]
         (let [actual (check-acls db subject effect resource access-token-effective-scope)]
           (if ok? (is (seq actual)) (is (not (seq actual)))))
 
-        "https://example.org/people/alice"                    ; subject
-        "https://example.org/effects/put-user-dir-resource"   ; effect
-        "https://example.org/~alice/foo.txt"                  ; resource
-        #{"userdir:write"} true )
+      ;; Alice can put a file to her user directory
+        "https://example.org/people/alice"
+        "https://example.org/effects/put-user-dir"
+        "https://example.org/~alice/foo.txt"
+        #{"userdir:write" "other:scope"} true
 
-    )
+        ;; Alice can't put a file to her user directory if the scope doesn't allow
+        ;; it (either the application is itself constrained, or she hasn't
+        ;; authorized the userdir:write scope on the application)
+        "https://example.org/people/alice"
+        "https://example.org/effects/put-user-dir"
+        "https://example.org/~alice/foo.txt"
+        #{"other:scope"} false
 
-  )
+        ;; Alice can't put a file to Bob's user directory
+        "https://example.org/people/alice"
+        "https://example.org/effects/put-user-dir"
+        "https://example.org/~bob/foo.txt"
+        #{"userdir:write"} false
+
+        ;; Alice can't put a file outside her user directory
+        "https://example.org/people/alice"
+        "https://example.org/effects/put-user-dir"
+        "https://example.org/index.html"
+        #{"userdir:write"} false
+
+        ;; Bob can put a file to his user directory
+        "https://example.org/people/bob"
+        "https://example.org/effects/put-user-dir"
+        "https://example.org/~bob/foo.txt"
+        #{"userdir:write"} true
+
+        ;; Bob can't put a file to Alice's directory
+        "https://example.org/people/bob"
+        "https://example.org/effects/put-user-dir"
+        "https://example.org/~alice/foo.txt"
+        #{"userdir:write"} false
+
+        ;; Carl cannot put a file to his user directory, as he hasn't been
+        ;; granted the put-user-dir effect.
+        "https://example.org/people/carl"
+        "https://example.org/effects/put-user-dir"
+        "https://example.org/~carl/foo.txt"
+        #{"userdir:write"} false)))
